a8a1b95cc23ced3d67b01b44f19b2eee281f93cf
Manish9737 <manishkumavat73@gmail.com>
2026-03-06 22:38:18 +0530

    adminAuth.js problem debug

--- server/middlewares/adminAuth.js
+++ server/middlewares/adminAuth.js
@@ -7,11 +7,17 @@ const JWT_SECRET = process.env.SECRET_KEY;
 const adminAuth = async (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer "))
+      return res.status(401).json({ success: false, message: "Unauthorized" });
+
     const token = authHeader?.split(" ")[1];
 
     if (!token) return res.status(401).json({ message: "Unauthorized" });
 
     const decoded = jwt.verify(token, JWT_SECRET);
+
+    console.log(decoded);
 
     if (decoded.role !== "admin") {
       return res.status(403).json({
